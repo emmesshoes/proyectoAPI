09ae060303e7bdd97fb36f0b23409bd2c24bb28d
Andrea <actassi@gmail.com>
2024-04-30 11:37:42 -0300

    trabajando en card

--- proyecto-api/src/pages/Home.jsx
+++ proyecto-api/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import BasicCard from '../utils/BasicCard';
+import { Container } from '@mui/material';
 
 export function Home() {
   return (
     <div>
+      <Container maxWidth="sm">
       <BasicCard />
+      </Container>
     </div>
   );
 }
