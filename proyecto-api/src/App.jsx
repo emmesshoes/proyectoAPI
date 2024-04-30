a2343557b93b2803e2ba27a067cab450dc177c44
Nahuel <cabralnahuel.dev@gmail.com>
2024-04-30 16:44:47 -0300

    probando rutas en app

--- proyecto-api/src/App.jsx
+++ proyecto-api/src/App.jsx
@@ -1,11 +1,17 @@
-import {Home} from './pages/Home'; 
-
+import { Home } from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-    return (
-        <Home/>
-      );
-    };
-    
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<h1>Hola esto es el login</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
